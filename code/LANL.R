4564f973bee528ab7f30116e5618942d578fd254
DylanPJackson <frprdjackson@gmail.com>
2020-09-23 00:17:45 -0400

    Updated LANL.R documentation

--- code/LANL.R
+++ code/LANL.R
@@ -1,25 +1,33 @@
+# Read in all of LANL's predictions into a list
 library(readr)
 model_name <- "LANL-GrowthRate"
 path <- sprintf("/home/dylan/repos/covid_factors/data/%s", model_name)
 files <- list.files(path = path, pattern = "*.csv", full.names = T)
 tbl <- lapply(files, read_csv)
 
+# Get the size of the list, because for whatever reason it's NULL atm
 size <- 0
 for (file in files){
     size <- size + 1
 }
 
+# Create empty predictions dataframe with blank dates and values
 dates <- 1:size
 class(dates) <- "Date"
 predictions <- data.frame(date = dates, value = numeric(size))
 
+# Populate the table with predicted U.S. cumulative deaths per day
 for (table_num in 1:size){
+    # Get the target date for this dataset
     date <- tbl[[table_num]]$target_end_date[1]
+    # Calculate the sum of all individual state's predictions
     value <- sum(tbl[[table_num]]$value[tbl[[table_num]]$target ==
         "1 wk ahead cum death" & tbl[[table_num]]$type == "point"])
     predictions$date[table_num] <- date
     predictions$value[table_num] <- value
 }
+
+# Save the plot to a .png file
 png('../visualizations/LANL_1week.png', width = 720, height = 720)
 start <- format(predictions$date[1], format = "%b %d, %Y")
 end <- format(predictions$date[size], format = "%b %d, %Y")
